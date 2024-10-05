48d5fdf788a3d677a23b1403c5b54b6fcbe9f2e3
manhattaa <fadynhatta@gmail.com>
2024-10-05 19:36:21 +0200

    Add project files.

---
+++ DigitalSign/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace DigitalSign
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string data = "Sensitive Information";
+
+            byte[] dataBytes = Encoding.UTF8.GetBytes(data);
+
+            using (RSA rsa = RSA.Create())
+            {
+                RSAParameters privateKey = rsa.ExportParameters(true);
+                RSAParameters publicKey = rsa.ExportParameters(false);
+
+                //sign data
+                byte[] signature = SignData(dataBytes, privateKey);
+
+                //verify signature
+                bool isValid = VerifySignature(dataBytes, signature, publicKey);
+
+                Console.WriteLine("Valid: " + isValid);
+            }
+        }
+
+        static byte[] SignData(byte[] data, RSAParameters privateKey)
+        {
+            using (RSA rsa = RSA.Create())
+            {
+                rsa.ImportParameters(privateKey);
+                return rsa.SignData(data, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+            }
+        }
+        static bool VerifySignature(byte[] data, byte[] signature, RSAParameters publicKey)
+        {
+            using (RSA rsa = RSA.Create())
+            {
+                rsa.ImportParameters(publicKey);
+                return rsa.VerifyData(data, signature, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+            }
+        }
+    }
+}
